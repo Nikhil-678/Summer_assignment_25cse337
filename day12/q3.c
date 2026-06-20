ed32c11f6a3bf1f4dda8f2827e069677a157be7f
nikhil <nikhil.cse2025@glbajajgroup.org>
2026-06-20 16:40:04 +0530

    Added day12 codes

---
+++ day12/q3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int maximum(int a, int b, int c)
+{
+    if (a >= b && a >= c)
+        return a;
+    else if (b >= a && b >= c)
+        return b;
+    else
+        return c;
+}
+int main()
+{
+    int num1, num2, num3;
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &num1, &num2, &num3);
+    printf("Maximum number = %d", maximum(num1, num2, num3));
+    return 0;
+}
