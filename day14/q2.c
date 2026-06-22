dbfe69fa0b649a1b1567c2c3a2226beee3c9be8c
nikhil <nikhil.cse2025@glbajajgroup.org>
2026-06-22 17:45:41 +0530

    Added day14 codes

---
+++ day14/q2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() 
+{
+    int arr[100], n, key, i, count = 0;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    printf("Enter array elements: ");
+    for(i = 0; i < n; i++) 
+    {
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter element to find frequency: ");
+    scanf("%d", &key);
+    for(i = 0; i < n; i++) 
+    {
+        if(arr[i] == key) {
+            count++;
+        }
+    }
+    if(count > 0)
+        printf("Frequency of %d = %d", key, count);
+    else
+        printf("Element not found");
+    return 0;
+}
